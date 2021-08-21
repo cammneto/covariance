2b397b8d2fdd7cffbc93fce184dc858f3d4ffe07
cammneto <cammneto@gmail.com>
2021-08-21 01:00:53 -0300

    deleted:    autovalores.dat 	deleted:    autovetores.dat 	deleted:    confirma.dat 	modified:   eigen.f90 	deleted:    matcov.dat 	deleted:    "m\303\251dias.dat" 	covmat.dat 	eigenvalues.dat 	eigenvectors.dat 	mean_data.dat

--- eigen.f90
+++ eigen.f90
@@ -1,25 +1,22 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!!!!  Programa para calcular a matriz covariância e  !!!!!!!
-!!!!!!  seus respectivos autovalores a partir de um  !!!!!!!!
-!!!!!!!!!  grupo de dados quaisquer  !!!!!!!!!!!!!!!!!!!!!!!!
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!!!!!!!!!!!  Autor:  Carlos Moreira de Melo Neto  !!!!!!!!!!!
+!!!!!!!!!  Code for calculating covariance matrix.  !!!!!!!!!
+!!!!!!!!!!!  Author: Carlos Moreira de Melo Neto  !!!!!!!!!!!
 !!!!!!!!!!!!!!!!  e-mail: cammneto@gmail.com !!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  program eigen
+  program covariance
   implicit none
    integer N, i
    real*8 covxx, covxy, covyy, xbar, ybar, x, y, x1, x2, x3, x4
    real L2, L1, D, C1, C2, C3, C4, C5, C6, C7, C8
 
    open(10,file='xy.dat')
-   open(30,file='matcov.dat')
-   open(40,file='médias.dat')
-   open(50,file='autovalores.dat')
-   open(60,file='autovetores.dat')
-   open(70,file='confirma.dat')
+   open(30,file='covmat.dat')
+   open(40,file='mean_data.dat')
+   open(50,file='eigenvalues.dat')
+   open(60,file='eigenvectors.dat')
+   open(70,file='testing_vectors.dat')
 
-     N=10 !Número de itens no grupo de dados
+     N=10 !length of a matrix line
      xbar=0
      ybar=0
 
@@ -46,7 +43,7 @@
      covxy = covxy + ((x-xbar)*(y-ybar))/(N-1)
    end do
 
-   write (30,*),'matriz covariância'
+   write (30,*),'covariance matrix'
    write (30,*), covxx, covxy
    write (30,*), covxy, covyy
 
@@ -56,35 +53,35 @@
 
     L2 = (covxx + covyy - D)/2
 
-   write (50,*),'Autovalores da matriz covariância'
+   write (50,*),'covariance matrix eigenvalues'
    write (50,*), L1
    write (50,*), L2
 
-!autovetores do autovalor L1
+!eigenvectors for L1 eigenvalue
     x1 = covxy/sqrt((covxy**2)+(L1-covxx)**2)
 
     x2 = (L1-covxx)*x1/covxy
 
-!autovetores do autovalor L2
+!eigenvector for L2 eigenvalue
     x3 =  covxy/sqrt((covxy**2)+(L2-covxx)**2)
 
     x4 = (L2-covxx)*x3/covxy
 
-   write (60,*),'Autovetores para o autovalor =' , L1
+   write (60,*),'eigenvector associater with L1 =' , L1
    write (60,*), x1, x2
-   write (60,*),'Autovetores para o autovalor =' , L2
+   write (60,*),'eigenvector associater with L2 =' , L2
    write (60,*), x3, x4
 
-!confirmação para o autovalor L1
+!cheking eigenvectors calculations for L1 eigenvalue
     C1 = covxx*x1 + covxy*x2
     C2 = covxy*x1 + covyy*x2
     C3 = x1*L1
     C4 = x2*L1
-   write (70,*), 'Se as igualdades estão corretas encontramos autovalores e autovetores corretamente.'
+   write (70,*), 'If the results are equal thus, the eigenvectors are correct.'
    write (70,*), C1, '=', C3
    write (70,*), C2, '=', C4
 
-!confirmação para o autovalor L2
+!cheking eigenvectors calculations for L2 eigenvalue
     C5 = covxx*x3 + covxy*x4
     C6 = covxy*x3 + covyy*x4
     C7 = x3*L2
